0130356c24e4a3f84eff0fe74fef7b8b343be043
Vanshika soni <106619869+vanshikasoni8@users.noreply.github.com>
2025-03-21 22:40:44 +0530

    [vanshika soni] Add: valid special characters

--- Email.js
+++ Email.js
@@ -1,6 +1,5 @@
-// Validation of Email
 const email = "abc.xyz@bridgelabz.co.in";
-const regex = /^abc\.[a-zA-Z0-9]+@bridgelabz\.[a-zA-Z]{2,}$/;
+const regex = /^abc([._\+\-]?[a-zA-Z0-9]+)*@bridgelabz\.[a-zA-Z]{2,}$/;
 
 if (regex.test(email)) {
   console.log("Valid Email");
